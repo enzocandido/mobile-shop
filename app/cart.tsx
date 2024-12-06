19c560eb86649bbcacdebfb7c01235f583d25730
Enzo Candido <ennzzc@gmail.com>
2024-12-05 23:23:39 -0300

    new version (build success web and mobile)

---
+++ app/cart.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonText } from "@/components/ui/button";
+
+export default function CartScreen() {
+  return (
+    <Button>
+      <ButtonText>
+        Text
+      </ButtonText>
+    </Button>
+  );
+}
